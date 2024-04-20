957be168eae4a930438a8d3f25f78b78c7bade14
mihir <mihir.vargante@factech.co.in>
2024-04-21 01:13:09 +0530

    Starting day of making portfolio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      lets start making our portfolio
+      </header>
+    </div>
+  );
+}
+
+export default App;
