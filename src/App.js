798ce88d8a2bafafef8e2b4a40bf3beddcf408da
mihir <mihir.vargante@factech.co.in>
2024-05-05 14:54:01 +0530

    Background and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Design from './components/Design';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      lets start making our portfolio
-      </header>
+    <div className="w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover relative overflow-hidden">
+      <div className="max-w-6xl mx-auto text-white">
+        <Navbar/>
+      </div>
+      <div className="w-full h-screen absolute top-0 left-0">
+        <Design />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        "banner-bg":"url('/src/assets/bannerBg.jpg')"
+      }
+    },
   },
   plugins: [],
 }
